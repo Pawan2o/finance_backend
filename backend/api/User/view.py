d5ad567206be79f527a8a9b5d1937a5985ac6f63
nhardy27 <neelprajapati229@gmail.com>
2026-03-06 14:10:04 +0530

    update view for user

--- backend/api/User/view.py
+++ backend/api/User/view.py
@@ -2,6 +2,8 @@ from api.User.model import CustomUser
 from api.User.serializers import UserSerializer
 from rest_framework.viewsets import ModelViewSet
 
+from rest_framework.permissions import IsAuthenticated
 class UserViewset(ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
