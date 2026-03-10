7b0d14c315ffcf2dc8af83b8e70130aa24395978
nhardy27 <neelprajapati229@gmail.com>
2026-03-10 11:56:59 +0530

    add permissions to payment method

--- backend/api/PaymentMethod/view.py
+++ backend/api/PaymentMethod/view.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-
+from rest_framework.permissions import IsAuthenticated
 from .model import PaymentMethod
 from .serializer import PaymentMethodSerializer
 
@@ -9,6 +9,7 @@ from .serializer import PaymentMethodSerializer
 class PaymentMethodViewSet(viewsets.ModelViewSet):
     queryset = PaymentMethod.objects.all()
     serializer_class = PaymentMethodSerializer
+    permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     search_fields = ['payment_method']
     filterset_fields = ['is_active', 'created_at']
