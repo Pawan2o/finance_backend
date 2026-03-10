296f026347e0854e20e4b88d917ad08db2c1e9e9
nhardy27 <neelprajapati229@gmail.com>
2026-03-10 16:55:57 +0530

    login complited

--- backend/security/views.py
+++ backend/security/views.py
@@ -1,9 +1,10 @@
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from rest_framework.permissions import AllowAny
-from .serializers import CustomTokenRefreshSerializer
+from .serializers import CustomTokenRefreshSerializer, CustomTokenObtainPairSerializer
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     permission_classes = [AllowAny]
+    serializer_class = CustomTokenObtainPairSerializer
 
 class CustomTokenRefreshView(TokenRefreshView):
     permission_classes = [AllowAny]
