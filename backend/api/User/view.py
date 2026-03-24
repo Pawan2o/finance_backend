80bfd7c5d513c43400b666dc62c059cdc2a97b89
nhardy27 <neelprajapati229@gmail.com>
2026-03-24 11:34:14 +0530

    add soft delete

--- backend/api/User/view.py
+++ backend/api/User/view.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class UserViewset(ModelViewSet):
@@ -15,3 +17,9 @@ class UserViewset(ModelViewSet):
     filterset_fields = ['is_active', 'is_staff', 'date_joined']
     ordering_fields = ['username', 'date_joined']
     ordering = ['-date_joined']
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_active = False
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
