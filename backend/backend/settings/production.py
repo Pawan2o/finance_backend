ee67cfd64e3d4c1ca005ce5814828ed3249ee440
Pawan Pareek <pawanpareekb20@gmail.com>
2026-05-14 15:53:34 +0530

    fix: refactor database configuration to use environment variables

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -3,28 +3,22 @@ import os
 from dotenv import load_dotenv
 load_dotenv(override=True)
 
-USER = os.getenv("USER")
-HOST = os.getenv("HOST")
-PORT = os.getenv("PORT")
-DB = os.getenv("DB")
-PASS = os.getenv("PASS")
-
 DEBUG = False
-ALLOWED_HOSTS = ["finance-backend-0bbu.onrender.com" ]
+ALLOWED_HOSTS = ["finance-backend-0bbu.onrender.com"]
 
-CORS_ALLOWED_ORIGINS = [ 
+CORS_ALLOWED_ORIGINS = [
     "https://placeholder.humbingo.in",
 ]
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': DB,
-         'HOST': HOST,
-         'PORT': PORT,
-         'USER':USER,
-         'PASSWORD': PASS
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT", "5432"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASS"),
+    }
 }
 
 BASE_URL = os.getenv("BASE_URL")
